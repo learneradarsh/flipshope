29262a99304bf8d08cc3dc99939e8e47be2762ce
LearnerAdarsh <learner.adarsh@gmail.com>
2017-02-01 12:49:05 +0530

    all linking done

---
+++ contactus.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+	
+<!-- Mirrored from ocean.tonytemplates.com/viktor/your_store_demo/html/contact.php by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 30 Jan 2017 11:09:00 GMT -->
+<head>
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<title>YOURStore - Responsive HTML5 Template</title>
+		<meta name="keywords" content="HTML5 Template" />
+		<meta name="description" content="YOURStore - Responsive HTML5 Template">
+		<meta name="author" content="etheme.com">
+		<link rel="shortcut icon" href="favicon.ico">
+		<!-- Mobile Specific Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- External Plugins CSS -->
+		<link rel="stylesheet" href="includes/external/slick/slick.css">
+		<link rel="stylesheet" href="includes/external/slick/slick-theme.css">
+		<link rel="stylesheet" href="includes/external/magnific-popup/magnific-popup.css">
+		<link rel="stylesheet" href="includes/external/nouislider/nouislider.css">
+		<link rel="stylesheet" href="includes/external/bootstrap-select/bootstrap-select.css">
+		<!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+		<link rel="stylesheet" type="text/css" href="includes/external/rs-plugin/css/settings.css" media="screen" />
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css">
+		<!-- Icon Fonts  -->
+		<link rel="stylesheet" href="font/style.css">
+		<!-- Head Libs -->
+		<!-- Modernizr -->
+		<script src="includes/external/modernizr/modernizr.js"></script>
+
+	</head>
+	<body>
+		
+		
+	    
+		<?php include_once 'includes/header.php'; ?>
+		<!-- breadcrumbs -->
+		<div class="breadcrumbs">
+			<div class="container">
+				<ol class="breadcrumb breadcrumb--ys pull-left">
+					<li class="home-link"><a href="index.php" class="icon icon-home"></a></li>										
+					<li class="active">Contact</li>
+				</ol>
+			</div>
+		</div>
+		<!-- /breadcrumbs --> 
+		<!-- CONTENT section -->
+		
+		<div id="pageContent">
+			<!-- map -->
+			<div class="content-bottom">
+				<div id="map"></div>
+			</div>				
+			<!-- /map -->
+			<section class="container">				
+				<div class="row">
+					<div class="col-md-3 col-sm-12">
+						<h2 class="text-uppercase title-bottom">CONTACTs</h2>
+						<ul class="list-icon">
+							<li>
+								<span class="icon icon-home"></span>
+								<strong>Address :</strong> 7563 St. Vicent Place, Glasgow
+							</li>
+							<li>
+								<span class="icon icon-call"></span>
+								<strong>Phone:</strong> +777 2345 7885
+							</li>
+							<li>
+								<span class="fa fa-fax"></span>
+								<strong>Fax:</strong> +777 2345 7886
+							</li>
+							<li>
+								<span class="icon icon-schedule"></span>
+								<strong>Hours:</strong> 7 Days a week from  10:00 am to 6:00 pm
+							</li>
+							<li>
+								<span class="icon icon-mail"></span>
+								<strong>E-mail:</strong> <a class="color" href="mailto:info@mydomain.com">info@mydomain.com</a>
+							</li>
+						</ul>
+						<div class="divider divider--sm"></div>
+						<div class="social-links social-links--large">
+							<ul>
+								<li><a class="icon fa fa-facebook" href="http://www.facebook.com/"></a></li>
+								<li><a class="icon fa fa-twitter" href="http://www.twitter.com/"></a></li>
+								<li><a class="icon fa fa-google-plus" href="http://www.google.com/"></a></li>
+								<li><a class="icon fa fa-instagram" href="https://instagram.com/"></a></li>
+								<li><a class="icon fa fa-youtube-square" href="https://www.youtube.com/"></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-9  col-sm-12">
+						<div class="divider divider--lg visible-xs"></div>
+						<h2 class="text-uppercase title-bottom">GET IN TOUCH WITH US</h2>
+						<form action="#" class="contact-form">
+							<!-- input -->
+							<div class="row">
+								<div class="col-md-4">
+									<div class="form-group">
+									    <label for="inputName">Name <sup>*</sup></label>
+									    <input type="text" class="form-control" id="inputName">
+									  </div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+									    <label for="inputEmail">Email <sup>*</sup></label>
+									    <input type="email" class="form-control" id="inputEmail">
+									  </div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+									    <label for="inputPhone">Phone </label>
+									    <input type="text" class="form-control" id="inputPhone">
+									  </div>
+								</div>
+							</div>
+							<!-- /input -->
+							<!-- textarea -->
+							<div class="form-group">
+							    <label for="textareaMessage">Message <sup>*</sup></label>
+							    <textarea  class="form-control" rows="12"  id="textareaMessage"></textarea>
+						   </div>
+						   <!-- /textarea -->
+						   <!-- button -->
+						   <div class="pull-right note">* Required Fields</div>
+						   <button class="btn btn--ys btn--xl btn-top" type="submit">Send message</button>
+						   <!-- /button -->						   
+						</form>						
+					</div>
+				</div>					
+			</section>
+		</div>
+		<!-- End CONTENT section --> 
+		<?php include_once 'includes/footer.php'; ?>
+
+	</body>
+
+</html>
