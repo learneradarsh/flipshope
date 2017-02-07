5907aeea504c2e84cfaf82910200e87da9efe042
LearnerAdarsh <learner.adarsh@gmail.com>
2017-02-07 14:28:07 +0530

    admin panel start

---
+++ flipshope_panel_of_admin/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>Flipshope || Admin Panel</title>
+		<!-- style sheets -->
+		<style rel="stylesheet" href="style.css"></style>
+	</head>
+	<body>
+		<div align="center">
+		<h3>CPANEL OF FLIPSHOPE</h3>
+		<form method="post" action="login.php" name="" id="" enctype="multipart-formdata">
+			<div class="cont">
+				<input type="text" name="username" id="username" placeholder="Username" required="required"/>
+			</div>
+			<div class="cont">
+				<input type="password" name="password" id="password" placeholder="password" required="required"/>
+			</div>
+			<div class="cont">
+			<button type="submit" id="">SUBMIT</button>
+			</div>
+			</form>
+		</div>
+	</body>
+</html>
